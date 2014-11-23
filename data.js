38678c85838edd5ff2e751052a19bb7a02e22e1f
Glyca <glycaa@gmail.com>
2014-11-23 11:32:29 +0100

    Nouveau workshop qui claque

--- data.js
+++ data.js
@@ -4,8 +4,8 @@ var workshops = [
         name: "Tennis avancé",
         user: 0,
         date: "vendredi 5 décembre 2014",
-        hour: "18h00",
-        duration: "1h00",
+        hour: "18:00",
+        duration: "1:00",
         place: "Strasbourg",
         details: "<p>Vivamus fermentum semper porta. Nunc diam velit, adipiscing ut tristique vitae, sagittis vel odio. <br /> Maecenas convallis ullamcorper ultricies. Curabitur ornare, ligula semper consectetur sagittis, nisi diam iaculis velit, id fringilla sem nunc vel mi. <br /> Nam dictum, odio nec pretium volutpat, arcu ante placerat erat, non tristique elit urna et turpis.</p>",
         checklist: "",
@@ -31,9 +31,9 @@ var workshops = [
         id: 1,
         name: "Initiation au Tennis",
         user: 0,
-        date: "vendredi 5 décembre 2014",
-        hour: "18h00",
-        duration: "1h00",
+        date: "vendredi 5 décembre",
+        hour: "18:00",
+        duration: "1:00",
         place: "Strasbourg",
         details: "Vivamus fermentum semper porta. Nunc diam velit, adipiscing ut tristique vitae, sagittis vel odio. Maecenas convallis ullamcorper ultricies.<br /> Curabitur ornare, ligula semper consectetur sagittis, nisi diam iaculis velit, id fringilla sem nunc vel mi. Nam dictum, odio nec pretium volutpat, arcu ante placerat erat, non tristique elit urna et turpis. <br /> Quisque mi metus, ornare sit amet fermentum et, tincidunt et orci. Fusce eget orci a orci congue vestibulum. Ut dolor diam, elementum et vestibulum eu, porttitor vel elit. Curabitur venenatis pulvinar tellus gravida ornare. Sed et erat faucibus nunc euismod ultricies ut id justo.",
         checklist: "",
@@ -54,9 +54,9 @@ var workshops = [
         id: 2,
         name: "Pétanque niveau mondial",
         user: 1,
-        date: "dimanche 7 décembre 2014",
-        hour: "18h00",
-        duration: "1h00",
+        date: "dimanche 7 décembre",
+        hour: "18:00",
+        duration: "1:00",
         place: "Strasbourg",
         details: "Suspendisse lectus leo, consectetur in tempor sit amet, placerat quis neque. Etiam luctus porttitor lorem, sed suscipit est rutrum non. <br /> Curabitur lobortis nisl a enim congue semper. Aenean commodo ultrices imperdiet. <br />Vestibulum ut justo vel sapien venenatis tincidunt. Phasellus eget dolor sit amet ipsum dapibus condimentum vitae quis lectus.",
         checklist: "",
@@ -70,6 +70,30 @@ var workshops = [
                 user: 3,
                 message: "JP est très intéressant et sympathique.",
                 rating: 1
+            }
+        ]
+    },
+    {
+        id: 3,
+        name: "Photo de Noël",
+        user: 5,
+        date: "dimanche 7 décembre",
+        hour: "17:00",
+        duration: "2:00",
+        place: "Place de Broglie, Strasbourg",
+        details: "Profitez de l'ouverture du marché de Noël pour venir apprendre les bases de la photo avec moi.<br><br>Le marché de Noël est une excellente occasion pour faire de superbes clichés, même pour les débutants !",
+        checklist: "",
+        price : 4,
+        checklist: [
+            "Votre appareil-photo",
+            "Un bon manteau",
+            "De la bonne humeur :)"
+        ],
+        comments: [
+            {
+                user: 4,
+                message: "Paulina est passionante et m'a donné envie de revenir au prochain en janvier !",
+                rating: 5
             }
         ]
     }
@@ -107,5 +131,12 @@ var users = [
         age: 34,
         bio: "J'aime bien la nation. J'espère que vous aussi !",
         rating: 4
+    },
+    {
+        id: 5,
+        name: "Paulina",
+        age: 24,
+        bio: "J'adore la finance !",
+        rating: 5
     }
 ];
